7574c7395199d1bfb34b174249ab34576d43c480
Yeahyun Kim <insidesy4@gmail.com>
2024-04-16 08:35:01 +0900

    [Feat] cancel-btn 함수 추가

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 $(document).ready(function() {
+    //[라이브러리 추가하기 버튼] mainPage -> createPage로 이동
     $('.create-btn').click(function() {
-        window.open('createPage.html', '_blank');
+        window.location.href = 'createPage.html';
+    });
+
+    //[취소 버튼] createPage / updatePage -> mainPage로 이동
+    $('.cancel-btn').click(function() {
+        window.location.href = 'index.html';
     });
 });
