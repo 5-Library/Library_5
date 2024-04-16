8d52de16beb560e02e295eaa1eab9c8768174212
Yeahyun Kim <insidesy4@gmail.com>
2024-04-16 09:07:45 +0900

    [Feat] updatePage 이동 함수 추가 및 html css 작업

--- js/index.js
+++ js/index.js
@@ -1,11 +1,16 @@
-$(document).ready(function() {
+$(document).ready(function () {
     //[라이브러리 추가하기 버튼] mainPage -> createPage로 이동
-    $('.create-btn').click(function() {
+    $('.create-btn').click(function () {
         window.location.href = 'createPage.html';
     });
 
+    //[라이브러리 추가하기 버튼] mainPage -> createPage로 이동
+    $('.update-btn').click(function () {
+        window.location.href = 'updatePage.html';
+    });
+
     //[취소 버튼] createPage / updatePage -> mainPage로 이동
-    $('.cancel-btn').click(function() {
+    $('.cancel-btn').click(function () {
         window.location.href = 'index.html';
     });
 });
