60b79dcdda0273964aff1753cbef56ecdd6a23f2
Yeahyun Kim <insidesy4@gmail.com>
2024-04-18 12:05:15 +0900

    [Feat] 폼 필드 입력 확인

--- js/index.js
+++ js/index.js
@@ -14,5 +14,37 @@ $(document).ready(function () {
         $(".delete-popup").hide();
         $('.profile-detail-info-wrap').show();
     });
-    
+
+    //[폼 필드 입력 확인] createPage / updatePage
+    function checkInputs() {
+        var allFilled = true;
+        $('.form-control, .form-select, .form-textarea').each(function () {
+            if ($(this).val() === '' || $(".form-select").val() === '일하는 스타일 선택') {
+                allFilled = false;
+                return false;
+            }
+        });
+
+        if (allFilled) {
+            $('#createBtn, #updateBtn').css({
+                'background-color': '#566FF5',
+                'color': '#ffffff'
+            });
+        } else {
+            $('#createBtn, #updateBtn').css({
+                'background-color': '#989898',
+                'color': '#f7f7f7'
+            });
+        }
+    }
+
+    // 페이지 로드 시 검사
+    checkInputs();
+
+    // 입력 필드 값 변경 시 검사
+    $('.form-control, .form-select, .form-textarea').on('change keyup', function () {
+        checkInputs();
+    });
+
+
 });
